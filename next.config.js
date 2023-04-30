658bfa373bf953f99f565d8b0708b0d0fcd6792a
Guillaume Lethuillier <66550865+glethuillier@users.noreply.github.com>
2023-04-30 11:26:26 +0200

    Fix CICD

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
     experimental: {
         appDir: true,
-    }
+    },
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
 };
 
 module.exports = nextConfig;
