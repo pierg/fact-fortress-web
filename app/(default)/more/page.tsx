605fd3915fed53bf32eee703306d27dfbbdb44a1
Guillaume Lethuillier <66550865+glethuillier@users.noreply.github.com>
2023-04-30 12:39:50 +0200

    More details: add page (draft)

--- app/(default)/more/page.tsx
+++ app/(default)/more/page.tsx
@@ -1,21 +1,50 @@
 export const metadata = {
-  title: "Sign In - Simple",
-  description: "Page description",
+  title: "Fact Fortress — More Information",
 };
 
 import Link from "next/link";
 
-export default function SignIn() {
+export default function More() {
   return (
     <section className="bg-gradient-to-b from-gray-100 to-white">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
           {/* Page header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h1 className="h1">Learn More</h1>
+          <div className="max-w-3xl mx-auto text-center">
+            <h1 className="h1">Learn More about Fact Fortress</h1>
           </div>
 
-          <div className="max-w-sm mx-auto text-center">Content</div>
+          <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6 md:mt-6" data-aos="fade-right">
+            <div className="md:pr-4 lg:pr-12 xl:pr-16 mb-8">
+              <h3 className="h3 mb-3 text-center">An On-Chain Zero-Knowledge Proof Solution to Fact-Checking</h3>
+              <p className="text-xl text-gray-600">Fact Fortress is a blockchain-integrated solution that democratizes the use of zero-knowledge proofs to ensure the integrity of private data. It provides a user-friendly interface for generating proofs off-chain through a circuit generator that leverages the Noir language and facilitates on-chain verification of the proofs on Ethereum.</p>
+              <p className="text-xl text-gray-600">By enabling the validation of data integrity without revealing the data itself, our solution offers a powerful mechanism for preserving the privacy of sensitive information while ensuring the security and transparency of blockchain technology.</p>
+              <p className="text-xl text-gray-600">
+                <Link
+                  href="https://github.com/pierg/fact-fortress-dapp"
+                  rel="noopener noreferrer" target="_blank"
+                  className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out"
+                >DApp
+                </Link>
+              </p>
+              <p className="text-xl text-gray-600">
+                <Link
+                  href="https://github.com/pierg/fact-fortress-compiler"
+                  rel="noopener noreferrer" target="_blank"
+                  className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out"
+                >Compiler
+                </Link>
+              </p>
+              <p className="text-xl text-gray-600">
+                <Link
+                  href="https://github.com/pierg/fact-fortress-web"
+                  rel="noopener noreferrer" target="_blank"
+                  className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out"
+                >Web
+                </Link>
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- app/(default)/people/page.tsx
+++ app/(default)/people/page.tsx
@@ -5,34 +5,44 @@ import Image from 'next/image'
 import Link from "next/link";
 import portraitPierGiuseppe from '@/public/images/people/piergiuseppe.jpeg'
 import portraitGuillaume from '@/public/images/people/guillaume.jpg'
+import Head from 'next/head'
 
-export default function SignIn() {
+export default function People() {
   return (
+
     <section className="bg-gradient-to-b from-gray-100 to-white">
+      <Head>
+        <meta name="robots" content="noindex,nofollow" />
+      </Head>
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
           {/* Page header */}
-          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+          <div className="max-w-3xl mx-auto text-center">
             <h1 className="h1">People</h1>
           </div>
 
           <div className="max-w-3xl mx-auto grid text-center">
             <div className="container">
-              <div className="photo-A"><Image className="mx-auto" src={portraitPierGiuseppe} width={230} height={300} priority alt="Portrait"/></div>
-              <div className="Photo-B"><Image className="mx-auto" src={portraitGuillaume} width={230} height={300} priority alt="Portrait"/></div>
+              <div className="photo-A"><Image className="mx-auto" src={portraitPierGiuseppe} width={230} height={300} priority alt="Portrait" /></div>
+              <div className="Photo-B"><Image className="mx-auto" src={portraitGuillaume} width={230} height={300} priority alt="Portrait" /></div>
               <div className="Name-A"><b>PierGiuseppe Mallozzi</b></div>
               <div className="Name-B"><b>Guillaume Lethuillier</b></div>
-              <div className="Occupation-A">Postdoctoral Researcher at UC Berkeley</div>
-              <div className="Occupation-B">Blockchain Software Developer at Blockdaemon</div>
-              <div className="Email-A">---</div>
-              <div className="Email-B">zkp@glthr.com</div>
+              <div className="Occupation-A"><i>Postdoctoral Researcher at UC Berkeley</i></div>
+              <div className="Occupation-B"><i>Blockchain Software Developer at Blockdaemon</i></div>
+              <div className="Email-A"></div>
+              <div className="Email-B"></div>
               <div className="Links-A"><ul className="nobull"><li>
                 <Link
                   href="https://pierg.github.io"
+                  rel="noopener noreferrer" target="_blank"
                   className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out"
-                >Personal website</Link></li><li>
+                >Personal website
+                </Link>
+              </li>
+                <li>
                   <Link
                     href="https://github.com/pierg"
+                    rel="noopener noreferrer" target="_blank"
                     className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out"
                   >Github</Link>
                 </li>
@@ -41,11 +51,13 @@ export default function SignIn() {
               <div className="Links-B"><ul className="nobull"><li>
                 <Link
                   href="https://www.linkedin.com/in/guillaume-lethuillier"
+                  rel="noopener noreferrer" target="_blank"
                   className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out"
                 >LinkedIn</Link></li>
                 <li>
                   <Link
                     href="https://github.com/glethuillier"
+                    rel="noopener noreferrer" target="_blank"
                     className="font-medium text-gray-600 hover:text-gray-900 px-5 py-3 flex items-center transition duration-150 ease-in-out"
                   >Github (personal)</Link>
                 </li>
