e9faa135410065f1525ad7d3e1575a45776176af
pier <piergiuseppe.mallozzi@gmail.com>
2023-04-30 13:01:27 -0700

    added text center

--- components/hero-fortress.tsx
+++ components/hero-fortress.tsx
@@ -46,14 +46,16 @@ export default function Hero() {
         {/* Hero content */}
         <div className="pt-32 pb-12 md:pt-40 md:pb-20">
           {/* Section header */}
-          <Image
-            className="mx-auto pb-12 md:pb-16"
-            data-aos="zoom-y-out"
-            src={Logo}
-            width={800}
-            priority
-            alt="Hero"
-          />
+          <div className="text-center">
+            <Image
+              className="mx-auto pb-12 md:pb-16"
+              data-aos="zoom-y-out"
+              src={Logo}
+              width={800}
+              priority
+              alt="Hero"
+            />
+          </div>
           <div className="text-center pb-12 md:pb-16">
             <h1
               className="text-4xl md:text-4xl font-extrabold leading-tighter tracking-tighter mb-4"
